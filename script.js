8c225abbaaa8ea8bea020b29c127995730755d6f
Reed Stewart <reedostewart@gmail.com>
2019-08-23 17:41:20 +1200

    Using the parallax library

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 window.addEventListener('mousemove', e => {
   let centerScreenX = window.innerWidth / 2;
-  if (e.x > centerScreenX) console.log('Mouse on right');
-  else console.log('Mouse on left');
+  if (e.x > centerScreenX) {
+    console.log('Mouse on right');
+  } else {
+    console.log('Mouse on left');
+  }
+
+  //  Try just moving the image to the left/right when on corresponding side - THEN add difference + ratios
+
+  //  Add/minus e.x from the center to find how far from the center the mouse is
 });
